a3bd12dcc749720910c859e7cecfa01b96912559
daniskhabirov <dakhabirov@gmail.com>
2023-12-02 14:30:19 +0300

    Initial commit.

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { Main } from "./pages/main/Main";
+
+createRoot(document.getElementById("root")).render(<Main />);
